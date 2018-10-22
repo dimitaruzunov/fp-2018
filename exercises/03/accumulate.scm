74ab823064a035c891c80303e9a9d2a5240612b9
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2018-10-23 00:22:52 +0300

    Add tests for 03 tasks

---
+++ exercises/03/accumulate.scm
@@ -0,0 +1,85 @@
+(require rackunit rackunit/text-ui)
+
+(define (identity x)
+  x)
+
+(define (1+ x)
+  (+ x 1))
+
+(define (square x)
+  (* x x))
+
+(define accumulate-tests
+  (test-suite
+   "Tests for accumulate"
+
+    (check = (accumulate + 0 identity 1 1+ 5) 15)
+    (check = (accumulate + 0 square 1 1+ 5) 55)
+
+    (check = (accumulate * 0 identity 1 1+ 5) 0)
+    (check = (accumulate * 1 identity 0 1+ 5) 0)
+    (check = (accumulate * 1 identity 1 1+ 5) 120)
+
+    (check = (accumulate (lambda (x acc)
+                           (if (even? x) (1+ acc) acc))
+                         0
+                         identity
+                         0 1+ 10)
+           6)
+    (check = (accumulate +
+                         0
+                         (lambda (x)
+                           (if (even? x) 1 0))
+                         0 1+ 10)
+           6)))
+
+(define accumulate-iter-tests
+  (test-suite
+   "Tests for accumulate-iter"
+
+    (check = (accumulate-iter + 0 identity 1 1+ 5) 15)
+    (check = (accumulate-iter + 0 square 1 1+ 5) 55)
+
+    (check = (accumulate-iter * 0 identity 1 1+ 5) 0)
+    (check = (accumulate-iter * 1 identity 0 1+ 5) 0)
+    (check = (accumulate-iter * 1 identity 1 1+ 5) 120)
+
+    (check = (accumulate-iter (lambda (x acc)
+                                (if (even? x) (1+ acc) acc))
+                              0
+                              identity
+                              0 1+ 10)
+           6)
+    (check = (accumulate-iter +
+                              0
+                              (lambda (x)
+                                (if (even? x) 1 0))
+                              0 1+ 10)
+           6)))
+
+(define sum-tests
+  (test-suite
+   "Tests for sum"
+
+    (check = (sum identity 1 1+ 5) 15)
+    (check = (sum square 1 1+ 5) 55)
+    (check = (sum (lambda (x)
+                    (if (even? x) 1 0))
+                  0 1+ 10)
+           6)))
+
+(define product-tests
+  (test-suite
+   "Tests for product"
+
+    (check = (product identity 0 1+ 5) 0)
+    (check = (product identity 1 1+ 5) 120)
+    (check = (product (lambda (x)
+                        (if (even? x) x 1))
+                      1 1+ 10)
+           3840)))
+
+(run-tests accumulate-tests)
+(run-tests accumulate-iter-tests)
+(run-tests sum-tests)
+(run-tests product-tests)
---
+++ exercises/03/double.scm
@@ -0,0 +1,20 @@
+(require rackunit rackunit/text-ui)
+
+(define (1+ x)
+  (+ x 1))
+
+(define (square x)
+  (* x x))
+
+(define double-tests
+  (test-suite
+   "Tests for double"
+
+    (check = ((double 1+) 0) 2)
+    (check = ((double square) 2) 16)
+    (check = ((double (double 1+)) 0) 4)
+    (check = (((double double) 1+) 0) 4)
+    (check = (((double (double double)) 1+) 0) 16)
+    (check = (((double (double double)) 1+) 5) 21)))
+
+(run-tests double-tests)
